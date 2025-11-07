239fe4a85c6f4ad074e2eb4a30d5bfa28215b451
ArthropodaEtAl <sonyeolchen@gmail.com>
2025-11-06 20:24:28 -0700

    Basic data process established

---
+++ data_process.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os 
+import plotly.express as px
+
+cwd = os.getcwd()
+data_wd = os.path.join(cwd, 'Timedata', 'Raw_Life_Data.csv')
+
+life_dataframe = pd.read_csv(data_wd)
+life_dataframe['Activity type'].unique()
+
+renaming_dictionary = {
+    'YouTube + computer' : 'Internet',
+    'Petting cat' : 'Pets',
+    'Art ' : 'Art',
+    'Music playing' : 'Piano',
+    'Work' : 'PhD',
+    'Side job' : 'Career',
+    'Romantic partner' : 'Romantic Partner'
+    }
+
+life_dataframe = life_dataframe.replace({'Activity type': renaming_dictionary})
+
+
+#might need to fix bc timestamp not datetime
+life_dataframe['From_dt'] = pd.to_datetime(life_dataframe['From'], format='%Y-%m-%d %H:%M:%S')
+life_dataframe['To_dt'] = pd.to_datetime(life_dataframe['To'], format='%Y-%m-%d %H:%M:%S')
+
+duplicate = life_dataframe[life_dataframe['From_dt'].dt.date != life_dataframe['To_dt'].dt.date].copy()
+duplicate_midnight = life_dataframe[life_dataframe['From_dt'].dt.date != life_dataframe['To_dt'].dt.date].copy()
+duplicate_morning = life_dataframe[life_dataframe['From_dt'].dt.date != life_dataframe['To_dt'].dt.date].copy()
+
+duplicate_midnight['To_dt'] = duplicate_midnight.apply(lambda x: pd.Timestamp(x['To_dt']).replace(
+    year = x['From_dt'].date().year,
+    month = x['From_dt'].date().month, 
+    day = x['From_dt'].date().day, 
+    hour= 23, 
+    minute= 59, 
+    second = 59),
+    axis = 1)
+duplicate_morning['From_dt'] = duplicate_morning.apply(lambda x: pd.Timestamp(x['From_dt']).replace(
+    year = x['To_dt'].date().year,
+    month = x['To_dt'].date().month, 
+    day = x['To_dt'].date().day, 
+    hour= 0, 
+    minute= 0, 
+    second = 1),
+    axis = 1)
+
+lifedataframe =life_dataframe.drop(duplicate.index)
+cleandataframe = pd.concat([lifedataframe, duplicate_midnight, duplicate_morning])
+
+assert len(cleandataframe[cleandataframe['From_dt'].dt.date != cleandataframe['To_dt'].dt.date]) == 0, "BAD DAYS"
+
+# add hour columns
+for i in range(24):
+    a = cleandataframe['From_dt'].apply(lambda x: x.replace(hour=i, minute=0, second=0))
+    b = cleandataframe['From_dt'].apply(lambda x: x.replace(hour=i, minute=59, second=59))
+    c = cleandataframe['From_dt']
+    d = cleandataframe['To_dt']
+    minbd = pd.concat([b, d], axis=1).apply(min, axis=1)
+    maxac = pd.concat([a, c], axis=1).apply(max, axis=1)
+    diff = (minbd - maxac).apply(lambda x: max(x, pd.Timedelta(0)))
+    cleandataframe[f"hour{i}"] = diff
+
+
+colors = {
+    'Piano': '#ff6c4a',
+    'Phone': '#000000',
+    'Internet': '#bdbdbd',
+    'Yoga': '#78ffff',
+    'Pets': '#3e2720',
+    'Walk': '#59c6df',
+    'Transport': '#c54167',
+    'Friends': '#d0030c',
+    'Read': '#8c7148',
+    'Write': '#a903fd',
+    'PhD': '#a58978',
+    'Chinese': '#06e872',
+    'Housework': '#8eb07a',
+    'Art': '#c91262',
+    'Career': '#5a5b60',
+    'Exercise': '#02bda4',
+    'Dnd': '#9467bd',
+    'Sleep': '#bcbd22',
+    'Chosing music': '#bcbd22',
+    'Job search': '#526e7b',
+    'Romantic Partner': '#fe01ff',
+    'Zone': '#ffaa06',
+}
+
+hour_columns = cleandataframe.columns
+
+clean_dataframe_bar = cleandataframe.groupby('Activity type')[hour_columns[-24:]].sum()
+clean_dataframe_bar= clean_dataframe_bar.apply(lambda x: x.apply(lambda y: y.total_seconds()/3600))
+clean_dataframe_bar=clean_dataframe_bar.transpose()
+fig_stacked, a_stacked = plt.subplots()
+clean_dataframe_bar.plot(ax = a_stacked, kind='bar', stacked = True, figsize = (6,6), legend=False, color =colors)
+fig_stacked.legend(loc='upper center', ncol = 6, bbox_to_anchor=(0.5, 1.10))
+
+
+# Graph by day of the week
+# by month
+# Graph by hour of the day
